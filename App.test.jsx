5894cda0f4b3298d8f3472cdbbdbe728fb481a61
Roman Ryzik <roman.ryzik@incorainc.com>
2025-06-19 21:26:20 +0300

    fixes for build

--- App.test.jsx
+++ App.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';        // <-- add this line
-import App from "./App";
+import App from "./src/App";
 
 test("renders Hello, Azure", () => {
   render(<App />);
